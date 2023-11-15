c708bcfb75fe2bf4a65f7fba4e451e6be35a3afe
uda224 <uda224@lehigh.edu>
2023-11-14 20:50:46 -0500

    record visit

--- PopulateData.java
+++ PopulateData.java
@@ -20,7 +20,7 @@ public class PopulateData {
                 
                 int role_type = printRoleMenu();
                 if(role_type == 1){
-                    propertyManagerMenu();
+                    propertyManagerMenu(conn);
                 } else if(role_type == 2){
                     tenantMenu();
                 } else if(role_type == 3){
@@ -59,7 +59,7 @@ public class PopulateData {
         return role;
     }
 
-    public static void propertyManagerMenu(){
+    public static void propertyManagerMenu(Connection conn){
         int option;
         Scanner scan = new Scanner(System.in);
         
@@ -69,7 +69,9 @@ public class PopulateData {
                 if(scan.hasNextInt()){
                     option = scan.nextInt();
                     if(option > 0 && option < 6){
-                        break;
+                        if(option == 1){
+                            recordVistorData(conn);
+                        }
                     }
                     System.out.println("Please enter a valid integer number between 1 and 5");
                 }
@@ -125,5 +127,95 @@ public class PopulateData {
             }
         }
     }
+
+    public static void recordVistorData(Connection conn){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Input prospective tenant's first name");
+        String first_name = scan.nextLine();
+
+        System.out.println("Input prospective middle name");
+        String middle_name = scan.nextLine();
+
+        System.out.println("Input prospective tenant's last name");
+        String last_name = scan.nextLine();
+
+        System.out.println("Input prospective tenant's address");
+        String address = scan.nextLine();
+
+        System.out.println("Input prospective tenant's city");
+        String city = scan.nextLine();
+
+        System.out.println("Input prospective tenant's state");
+        String state = scan.nextLine();
+
+        System.out.println("Input prospective tenant's country");
+        String country = scan.nextLine();
+
+        System.out.println("Input prospective tenant's zipcode");
+        String zipcode = scan.nextLine();
+
+        System.out.println("Input prospective tenant's phone number");
+        String phone_number = scan.nextLine();
+
+        System.out.println("Input prospective tenant's email");
+        String email = scan.nextLine();
+
+        System.out.println("Input prospective tenant's age");
+        int age = scan.nextInt();
+        scan.nextLine(); // to consume \n char 
+
+        System.out.println("Input prospective tenant's gender");
+        String gender = scan.nextLine();
+
+        System.out.println("Input prospective tenant's credit score");
+        int credit_score = scan.nextInt();
+        scan.nextLine(); // to consume \n char 
+
+        System.out.println("Input yes or no if the prospective tenant has a pet or not");
+        String has_pet = scan.nextLine();
+
+        try{
+            // TO DO: ERROR HANDLING 
+            PreparedStatement preparedStatement1 = conn.prepareStatement("Insert into Tenant (first_name, middle_name, last_name, address, city, state, country, zipcode, phone_num, email, age, gender, credit_score, has_pet) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            preparedStatement1.setString(1, first_name);
+            preparedStatement1.setString(2, middle_name);
+            preparedStatement1.setString(3, last_name);
+            preparedStatement1.setString(4, address);
+            preparedStatement1.setString(5, city);
+            preparedStatement1.setString(6, state);
+            preparedStatement1.setString(7, country);
+            preparedStatement1.setString(8, zipcode);
+            preparedStatement1.setString(9, phone_number);
+            preparedStatement1.setString(10, email);
+            preparedStatement1.setInt(11, age);
+            preparedStatement1.setString(12, gender);
+            preparedStatement1.setInt(13, credit_score);
+            preparedStatement1.setString(14, has_pet);
+            preparedStatement1.executeUpdate();                   
+            preparedStatement1.close();
+        } catch(SQLException se){
+            se.printStackTrace();
+        }
+
+        System.out.println("Input visit date in the form DD-MMM-YYY: ");
+        String date = scan.nextLine();
+        try{
+            // TO DO: ERROR HANDLING 
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT count(*) from Tenant");
+            ResultSet rs = preparedStatement.executeQuery();
+            rs.next();
+            int count = rs.getInt(1); // OH: HOW TO GET THE TENANT_ID HERE?
+            PreparedStatement preparedStatement2 = conn.prepareStatement("Insert into ProspectiveTenant (visit_date) values (?)");
+            preparedStatement2.setString(1, date);
+            preparedStatement2.executeUpdate();                   
+            preparedStatement2.close();
+        } catch(SQLException se){
+            se.printStackTrace();
+        }
+        
+
+    }
+
 
 }
